928b7b544fcdf3e8fdae639b76e749954ec8b7bb
alicekamil <alice.anastasiak@outlook.com>
2023-10-24 13:05:40 +0200

    Initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rB;
+    private void Start()
+    {
+        rB = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rB.velocity = movement;
+
+    }
+}
